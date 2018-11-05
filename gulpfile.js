f46ce26040bcdfe0415c8d227ef3bd7c8064c2d2
Стас <cjb2018@yandex.ru>
2018-11-05 22:20:30 +0600

    second commit

---
+++ gulpfile.js
@@ -0,0 +1,44 @@
+global.$ = {
+    gulp: require("gulp"),
+    browsersync: require("browser-sync").create(),
+    packageJson: require('./package.json'),
+    autoprefixer: require("gulp-autoprefixer"),
+    babel: require("gulp-babel"),
+    uglify: require("gulp-uglify"),
+    concat: require("gulp-concat"),
+   // sass: require("gulp-sass"),
+    stylus : require('gulp-stylus'),
+	gcmq : require('gulp-group-css-media-queries'),
+    mincss: require("gulp-clean-css"),
+    pug : require('gulp-pug'),
+    sourcemaps: require("gulp-sourcemaps"),
+    rename: require("gulp-rename"),
+    imagemin: require("gulp-imagemin"),
+    pngquant: require("imagemin-pngquant"),
+    imageminJpegRecompress: require("imagemin-jpeg-recompress"),
+    favicons: require("gulp-favicons"),
+    iconfont: require("gulp-iconfont"),
+    iconfontcss: require("gulp-iconfont-css"),
+	svgSprite: require("gulp-svg-sprites"),
+    replace: require("gulp-replace"),
+    rigger: require("gulp-rigger"),
+    newer: require("gulp-newer"),
+    plumber: require("gulp-plumber"),
+    debug: require("gulp-debug"),
+    watch: require("gulp-watch"),
+    clean: require("gulp-clean"),
+
+    path: {
+        tasks: require("./gulp/config.js")
+    }
+};
+
+$.path.tasks.forEach(function(taskPath) {
+    require(taskPath)();
+});
+
+// BUILD
+$.gulp.task("default", $.gulp.series("clean", "sprite", "iconfont", "libs",
+    $.gulp.parallel("html", "styles", "favicons", "images", "scripts", "server_conf"),
+    $.gulp.parallel("watch", "serve")
+));
